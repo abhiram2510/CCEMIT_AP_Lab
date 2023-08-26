c75cb33658aea204dfe325d764d06c732a6f04a5
Abhiram Patruni <abhirampatruni03@gmail.com>
2023-08-26 09:39:49 +0530

    Delete Lab 2 temp directory

--- Lab 2 temp/p1.py
+++
@@ -1,8 +0,0 @@
-str=input("enter the String : ")
-words = []
-words = str.split()
-myDict = {}
-for key in words:
-    myDict[key] = words.count(key)
-print("Dict Items  :  ",  myDict)
-print("total unique items count: ",len(myDict))
