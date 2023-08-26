a206426d5703b370c985f56490318c5a20cff749
Abhiram Patruni <abhirampatruni03@gmail.com>
2023-08-26 09:29:28 +0530

    Update Lab2Q2.py

--- Lab 2/Lab2Q2.py
+++ Lab 2/Lab2Q2.py
@@ -1,61 +1,33 @@
-#Taking the total number of rows and columns for each matrix--------
-row1=int(input("Enter the number of rows in the first matrix:"))
-column1=int(input("Enter the number of columns in the first matrix:"))
-row2=int(input("Enter the number of rows in the second matrix:"))
-column2=int(input("Enter the number of columns in the second matrix:"))
-mat1=[]
-mat2=[]
-
-#Taking input for both matrices
+row1 = int(input("Enter the number of rows in the first matrix:"))
+column1 = int(input("Enter the number of columns in the first matrix:"))
+row2 = int(input("Enter the number of rows in the second matrix:"))
+column2 = int(input("Enter the number of columns in the second matrix:"))
+dict1 = {}
+dict2 = {}
 
 for i in range(row1):
-    rows=[]
     for j in range(column1):
-        element= int(input("Enter the elements row wise:"))
-        rows.append(element)
-    mat1.append(rows)
-
-for i in range(row2):
-    rows=[]
-    for j in range(column2):
-        element= int(input("Enter the elements row wise:"))
-        rows.append(element)
-    mat2.append(rows)
-
-
-
-#Putting the values into a dictionary
-dict1={}
-dict2={}
-matrix1=[]
-matrix2=[]
-for i in range(row1):
-    for j in range(column1):
-        if mat1[i][j] != 0:
-            dict1[i,j] = mat1[i][j]
+        element = int(input("Enter the elements row wise:"))
+        if element != 0:
+            dict1[i, j] = element
 
 for i in range(row2):
     for j in range(column2):
-        if mat2[i][j] != 0:
-            dict2[i,j]=mat2[i][j]
+        element = int(input("Enter the elements row wise:"))
+        if element != 0:
+            dict2[i, j] = element
 
-print(dict1)
-print(dict2)
-#adding the values whose keys are the same
-
-arr=[];
-
-for key1 in dict1:
-    for key2 in dict2:
-        if key1 == key2:
-            arr.append(dict1[key1]+dict2[key2])
-            break
-        elif key1 in dict1:
-            arr.append(dict1[key1])
-            break
-        else:
-            arr.append(dict2[key2])
-            break
-
-print(arr)
-
+rows = max(row1, row2)
+columns = max(column1, column2)
+result = [[0 for _ in range(columns)]for _ in range(rows)]
+for i in range(rows):
+    for j in range(columns):
+        if (i, j) in dict1:
+            result[i][j] += dict1[(i, j)]
+        if (i, j) in dict2:
+            result[i][j] += dict2[(i, j)]
+print("Result matrix:")
+for i in range(rows):
+    for j in range(columns):
+        print(result[i][j], end="\t")
+    print()
